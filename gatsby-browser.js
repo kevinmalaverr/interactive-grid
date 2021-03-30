b43413b55fd06b2e63fa43e2fd1292c6313ba60f
kevinmalaverr <kevinmalaverr@gmail.com>
2021-03-30 17:41:49 -0500

    added ssr SEO

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import Layout from './src/components/Layout';
-import './src/styles/global.css';
-import 'animate.css';
+import React from "react"
+import Layout from "./src/components/Layout"
+import "./src/styles/global.css"
+import "animate.css"
 
-export const wrapPageElement = ({ element, props }) => {
-  return (
-    <>
-      <Layout {...props}>{element}</Layout>
-    </>
-  );
-};
+export const wrapPageElement = ({ element, props }) => (
+  <>
+    <Layout {...props}>{element}</Layout>
+  </>
+)
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Layout from './src/components/Layout';
+import React from "react"
+import Layout from "./src/components/Layout"
 
-export const wrapPageElement = ({ element, props }) => {
-  return (
-    <>
-      <Layout {...props}>{element}</Layout>
-    </>
-  );
-};
+export const wrapPageElement = ({ element, props }) => (
+  <>
+    <Layout {...props}>{element}</Layout>
+  </>
+)
 
 export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   setPostBodyComponents([
     <div key="animations" id="animation-portal" style={{ zIndex: 100 }} />,
-  ]);
-};
+  ])
+}
